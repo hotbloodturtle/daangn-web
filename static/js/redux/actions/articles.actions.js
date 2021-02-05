e88c44dde9c7faa187dab08b568e60a0476b302f
HaeSeok Kang <hotbloodturtle@gmail.com>
2021-02-05 19:51:22 +0900

    fix - typing error

--- static/js/redux/actions/articles.actions.js
+++ static/js/redux/actions/articles.actions.js
@@ -4,10 +4,6 @@ import { publish } from "../pubsub.js";
 
 export const HOT_ARTICLE_LIST = "HOT_ARTICLE_LIST";
 
-export const test = {
-  HOT_ARTICLE_LIST: HOT_ARTICLE_LIST,
-};
-
 export const hotArticlesAction = () => {
   const dispatch = (hotArticles) => {
     articlesReducer({ type: HOT_ARTICLE_LIST, payload: { hotArticles } });
