6d13e595332835b7d77685140c02565e0ec543ee
HaeSeok Kang <hotbloodturtle@gmail.com>
2021-02-19 13:21:24 +0900

    fix - clear_database

--- clear_database.py
+++ clear_database.py
@@ -1,36 +1,42 @@
 import os
+import sys
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-os.system('python manage.py migrate admin zero')
-os.system('python manage.py migrate auth zero')
-os.system('python manage.py migrate contenttypes zero')
-os.system('python manage.py migrate sessions zero')
+source_dir = os.path.dirname(os.path.abspath(__file__))
+print(source_dir)
+sys.path.append(source_dir)
 
-apps = [
-    'articles',
-]
-for app in apps:
-    os.system(f'python manage.py migrate {app} zero')
-    os.system(f'rm -r {app}/migrations')
-    os.system(f'mkdir {app}/migrations')
-    os.system(f'touch {app}/migrations/__init__.py')
+import django
+django.setup()
 
+from django.apps import apps
 from django.db import connection
+
 with connection.cursor() as cursor:
-    cursor.execute('DROP TABLE django_migrations')
+    DJANGO_DB_NAME = os.getenv('DJANGO_DB_NAME') or 'db'
+
+    # postgres
+    cursor.execute(f'DROP SCHEMA public CASCADE;')
+    cursor.execute(f'CREATE SCHEMA public;')
+
+    # mysql
+    # cursor.execute(f'DROP DATABASE {DJANGO_DB_NAME}')
+    # cursor.execute(f'CREATE DATABASE {DJANGO_DB_NAME}')
+
+for app in apps.get_app_configs():
+    os.system(f'rm -r {app.label}/migrations')
+    os.system(f'mkdir {app.label}/migrations')
+    os.system(f'touch {app.label}/migrations/__init__.py')
 
 os.system('python manage.py makemigrations')
 os.system('python manage.py migrate')
 
 
-import django
-django.setup()
-
-
+# dummy data
 from django.contrib.auth import get_user_model
 User = get_user_model()
 user = User.objects.create_user(username='admin', email='admin@admin.com', password='qwer1234')
-
 
 from articles.models import Article
 for i in range(10):
